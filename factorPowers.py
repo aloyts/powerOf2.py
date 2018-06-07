68090191037f4033853ed1a864e8ce6010e95d5d
Simon Aloyts <37598853+aloyts@users.noreply.github.com>
2018-06-07 07:17:31 -0500

    Update factorPowers.py

--- factorPowers.py
+++ factorPowers.py
@@ -1,14 +1,23 @@
-import math
-#powInt = 4294967295
-#powInt = 895
-powCount = powInt
-maxPower = math.floor((math.log2(powInt)))
+def factorPowers(powInt):
 
-print("maxPower=", maxPower, "\n")
+    # =============================================
+    # Author: Aloyts
+    # Create date: 2018 June 06
+    # Description: Factoring powers of 2
+    # =============================================
 
-if (math.log2(powInt)%2 != 0) :
-    print(powInt, "=\n")
-    for i in range(maxPower, -1, -1):
-        if powCount-math.pow(2, i) >= 0:
-            powCount=powCount-math.pow(2, i)
-            print("(2^",i,") = ", math.pow(2, i))
+    powCount = powInt
+    maxPower = math.floor((math.log2(powInt)))
+
+    print("maxPower=", maxPower, "\n")
+
+    if (math.log2(powInt)%2 != 0) :
+        print(powInt, "=\n")
+        for i in range(maxPower, -1, -1):
+            if powCount-math.pow(2, i) >= 0:
+                powCount=powCount-math.pow(2, i)
+                print("(2^",i,") = ", math.pow(2, i))
+                
+#factorPowers(4294967295) #sample run 2^32-1
+#factorPowers(895)        #sample random number
+#factorPowers(33792)      #sample 2^10 + 2^15
