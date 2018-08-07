602bd42573973c0828f9b391191e9bba3249ade5
Simon Aloyts <37598853+aloyts@users.noreply.github.com>
2018-08-07 07:52:13 -0500

    factorPowers.go

---
+++ factorPowers.go
@@ -0,0 +1,37 @@
+package main
+
+import ("fmt")
+import ("math")
+
+func main() {
+	var factors [50]float64
+	
+	var totalVal float64 = 17
+	var powCount = totalVal
+	
+	var maxPower float64
+	var rem float64
+	
+	//var i float64
+	var j int = 0
+	
+	maxPower = math.Floor(math.Log2(totalVal))
+	rem = totalVal - math.Pow(2, maxPower)
+	fmt.Println(maxPower)
+	fmt.Println(rem)
+	
+	if(rem!=0){
+		for i:=maxPower; i >= 0 ; i-- {
+			if(powCount - math.Pow(2,i) >= 0){
+          			factors[j] = math.Pow(2,i)
+			        powCount -= math.Pow(2,i)
+				j++
+				fmt.Printf("nextPow=%.0f\n", math.Pow(2,i))
+				
+				fmt.Println("j=", j)
+        		}
+		}
+	}
+
+}
+
